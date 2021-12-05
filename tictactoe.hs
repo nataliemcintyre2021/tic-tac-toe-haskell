595e02bbc6804e62880876e69ee30449b6a5f576
nataliemcintyre2021 <nataliemcintyre2021@gmail.com>
2021-12-05 16:05:21 -0700

    Practice with different types, possible gameboard layout

--- tictactoe.hs
+++ tictactoe.hs
@@ -2,9 +2,16 @@ type Name = String
 name :: Name
 name = "Natalie"
 
+-- main :: IO ()
+-- main = putStrLn ("Hello there "
+--                 ++ show (name)
+--                 ++ "!")
 
+theAnswerNumber :: Integer
+theAnswerNumber = 33 * 398
 
 main :: IO ()
-main = putStrLn ("Hello there "
-                ++ show (name)
-                ++ "!")
+main = putStrLn gameBoard
+
+gameBoard :: String
+gameBoard = " 1 | 2 | 3 \n 4 | 5 | 6 \n 7 | 8 | 9 "
